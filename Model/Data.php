cd3c9d5f26eb81486cb6f30277fd0d4e507deeec
krabs <krabs@live.cn>
2019-01-17 13:06:17 +0800

    增加论坛快捷读写数据Model

---
+++ Model/Data.php
@@ -0,0 +1,37 @@
+<?php
+namespace Model;
+use HY\Model;
+!defined('HY_PATH') && exit('HY_PATH not defined.');
+/**
+ * 本Model没有对应数据表，用于集合论坛数据快捷读写，多数采用缓存。
+ * 函数列表：
+ * get_post_data($pid) //获取评论数据，评论不存在返回false，反之获得数据array
+ * 
+*/
+class Data extends Model {
+    public $CacheObj;
+    public function __construct(){
+        $this->CacheObj = cache(array());
+    }
+    /**
+     * 获取某评论数据
+     * @access public
+     * @param int $pid 评论pid
+     * @return array | boolean
+    */
+    public function get_post_data($pid){
+        $PostData = $this->CacheObj->get('post_data_'.$pid);
+        if(empty($PostData) || DEBUG){
+            //{hook m_data_get_post_data_1}
+            $PostData = S('Post')->find("*",['pid'=>$pid]);
+            if(empty($PostData))
+                return false;
+            $this->CacheObj->set('post_data_'.$pid,$PostData);
+            //{hook m_data_get_post_data_2}
+        }
+        return $PostData;
+    }
+   
+    //{hook m_data_fun}
+    
+}
