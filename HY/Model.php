c10037d9b9585c806900e683dc075a28e8e860ff
krabs <krabs@live.cn>
2019-04-20 14:17:28 +0800

    增加查询缓存

--- HY/Model.php
+++ HY/Model.php
@@ -13,6 +13,10 @@ use PDO;
 class Model {
     public $pdo = false; 
     public $table;
+    public $cache_mode = false;
+    public $cache_key;
+    public $cache_expire = NULL;
+    public $CacheObj;//用于数据缓存
     function __construct($name,$more_sql_name = '') {
         static $pdo_obj =array();
         $sql_more_bool = false;
@@ -65,8 +69,26 @@ class Model {
     }
     //查询数据 要查询的字段名.    查询的条件.
     public function select($join, $columns = null, $where = null){
-        return $this->pdo->select($this->table,$join,$columns,$where);
-        
+        if($this->cache_mode){
+            $this->cache_mode = false;
+            if($this->cache_key === true){
+                $this->cache_key = md5('select_'.$this->table.to_guid_string($join).to_guid_string($columns).to_guid_string($where));
+            }
+            //查询缓存
+            $result =$this->CacheObj->get($this->cache_key);
+            if($result !== false){
+                return $result;
+            }
+            //重新查询
+            $result = $this->pdo->select($this->table,$join,$columns,$where);
+            if(!$result){//不记录 查询失败
+                return $result;
+            }
+            $this->CacheObj->set($this->cache_key,$result,$this->cache_expire);
+            return $result;
+        }
+        $result = $this->pdo->select($this->table,$join,$columns,$where);
+        return $result;
     }
     public function update($data, $where=null){
         return $this->pdo->update($this->table,$data,$where);
@@ -75,12 +97,30 @@ class Model {
         return $this->pdo->delete($this->table,$where);
     }
     public function get($join = null, $column = null, $where = null){
-        return $this->pdo->get($this->table,$join,$column,$where);
-        
+        if($this->cache_mode){
+            $this->cache_mode = false;
+            if($this->cache_key === true){
+                $this->cache_key = md5('get_'.$this->table.to_guid_string($join).to_guid_string($column).to_guid_string($where));
+            }
+            //查询缓存
+            $result =$this->CacheObj->get($this->cache_key);
+            if($result !== false){
+                return $result;
+            }
+            //重新查询
+            $result = $this->pdo->get($this->table,$join,$column,$where);
+            if(!$result){//不记录 查询失败
+                return $result;
+            }
+            $this->CacheObj->set($this->cache_key,$result,$this->cache_expire);
+            return $result;
+        }
+
+        $result = $this->pdo->get($this->table,$join,$column,$where);
+        return $result;
     }
     public function find($join = null, $column = null, $where = null){
-        return $this->pdo->get($this->table,$join,$column,$where);
-        
+        return $this->get($join,$column,$where);
     }
     public function replace($columns, $search = null, $replace = null, $where = null){
         return $this->pdo->replace($this->table,$columns, $search, $replace, $where);
@@ -118,6 +158,27 @@ class Model {
     }
     public function id(){
         return $this->pdo->id();
+    }
+    public function cache($name,$expire=NULL){
+        static $CacheObj = false;
+        if(!$CacheObj){
+            $CacheObj = cache(array());
+            echo '缓存第一次<br>';
+        }
+        $this->CacheObj = $CacheObj;
+        $this->cache_mode = true;
+        $this->cache_key = $name;
+        $this->cache_expire = $expire;
+        //echo to_guid_string(null) . '<br>';
+        // $v = $CacheObj->get($name);
+        // if(!empty($v)){
+        //     echo '输出缓存：'.$v.'<br>';
+        // }else{
+        //     $CacheObj->set($name,'test',10);
+        //     echo '开始缓存<br>';
+        // }
+        
+        return $this;
     }
 
 
