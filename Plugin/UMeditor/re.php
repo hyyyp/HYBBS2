154df6329f3e53377eea16eb6af43afe57cd38d0
krabs <krabs@live.cn>
2019-05-08 21:34:06 +0800

    删除旧编辑器

--- Plugin/UMeditor/re.php
+++
@@ -1,6 +0,0 @@
-<?php
-return array(
-	'View/hy_boss/thread_index.html'=>array(
-		'a1'=>'b1'
-	)
-);
--- Plugin/hy_moblie_editor/conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-    'name' => 'HY移动编辑器',
-    'user' => 'admin',
-    'icon' => '',
-    'mess' => '手机移动设备编辑器',
-    'version' => '2.2',
-);
