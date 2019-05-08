fce54b594544badb63c162b64a9ebb9b7c701664
krabs <krabs@live.cn>
2019-05-08 21:28:52 +0800

    Update Chat.php

--- Model/Chat.php
+++ Model/Chat.php
@@ -3,19 +3,18 @@ namespace Model;
 use HY\Model;
 !defined('HY_PATH') && exit('HY_PATH not defined.');
 class Chat extends Model{
-
-
 	// $uid1= 接收者
 	// $uid2= 发送者 , 0=系统消息
 	public function send($uid1,$uid2,$content){
 		//{hook m_chat_send_1}
+		//插入聊天内容
 		$this->insert(
-			array(
+			[
 				'uid1'=>$uid1,
 				'uid2'=>$uid2,
 				'content'=>$content,
 				'atime'=>NOW_TIME
-			)
+			]
 		);
 		//{hook m_chat_send_2}
 		$Friend = M("Friend");
