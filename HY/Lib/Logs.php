89ec1f8807e15d41d6ddd7a9754b003e1cc11a34
krabs <krabs@live.cn>
2019-06-22 21:53:29 +0800

    修复框架日志类

--- HY/Lib/Logs.php
+++ HY/Lib/Logs.php
@@ -6,9 +6,13 @@ class Logs{
 	public function log($content){
 		if(!is_writable(TMP_PATH))
 			return;
-		$tmp = trim(file_get_contents(TMP_PATH . 'log.php'));
-		if(!is_file(TMP_PATH . 'log.php') || empty($tmp))
-			file_put_contents(TMP_PATH . 'log.php','<?php die(); ?>');
-		file_put_contents(TMP_PATH . 'log.php',($content), FILE_APPEND);
+		$LOG_FILE_PATH = TMP_PATH . 'log.php';
+		$log_content = '';
+		if(is_file($LOG_FILE_PATH))
+			$log_content = trim(file_get_contents($LOG_FILE_PATH));
+
+		if(!is_file($LOG_FILE_PATH) || empty($log_content))
+			file_put_contents($LOG_FILE_PATH,'<?php die(); ?>');
+		file_put_contents($LOG_FILE_PATH,($content), FILE_APPEND);
 	}
 }
