5712b199a696ec94db47066627d403015ff57edd
XuanRanZhiLian <33628985+XuanRanZhiLian@users.noreply.github.com>
2019-10-12 15:44:00 +0800

    Update thread_list.php

--- View/hybbs/thread_list.php
+++ View/hybbs/thread_list.php
@@ -7,7 +7,7 @@
             {if !$v['top']}
 			<a class="forum-name" href="{php HYBBS_URL('forum',$v['fid']);}">[ {php echo forum($forum,$v['fid'],'name');} ]</a>
             {/if}
-			<a style="{if $v['top']==2}font-weight: bold;color: #09C;{elseif $v['top']==1}font-weight: bold;color: #CE792D;{/if}" class="thread-title" href="{php HYBBS_URL('thread',$v['tid']);}">{if $v['top']==2}<span class="qzd">{$_LANG['全站置顶']}</span>{elseif $v['top']==1}<span class="fzd">{$_LANG['本版置顶']}</span>{/if}{$v.title}{if $v['state']}<span title="{$_LANG['禁止回复']}" style="color: brown;"> - {$_LANG['已锁定']}</span>{/if}</a>
+			<a style="{if $v['top']==2}font-weight: bold;color: #09C;{elseif $v['top']==1}font-weight: bold;color: #CE792D;{/if}" class="thread-title" href="{php HYBBS_URL('thread',$v['tid']);}">{if $v['top']==2}<span class="qzd">{$_LANG['全站置顶']}</span>{elseif $v['top']==1}<span class="fzd">{$_LANG['本版置顶']}</span>{/if}{if $v['digest']}<span title="{$_LANG['精']}" style="color: red;">{$_LANG['精']}</span>{/if}{$v.title}{if $v['state']}<span title="{$_LANG['禁止回复']}" style="color: brown;"> - {$_LANG['已锁定']}</span>{/if}</a>
 
 		</h4>
         <div class="meta">
