cb8d95786cbd5264c21c1d1bb3737c9d53245ea6
XuanRan <pay@ktwap.net>
2019-10-13 18:06:00 +0800

    Update User.php

--- Action/User.php
+++ Action/User.php
@@ -86,8 +86,8 @@ class User extends HYBBS {
             if (empty($username)){
                 return $this->message('用户名不能为空!');
             }
-            $userSql = S("User");
-            if($userSql->has(['user'=>$username])){
+            $userModel = M("User");
+            if($userModel->is_user($username)){
                 return $this->message('该用户名以存在!');
             }
             $UserLib = L("User");
@@ -98,7 +98,7 @@ class User extends HYBBS {
 
             //{hook a_user_edit_5}
 
-            $userSql->update(array(
+            $userModel->update(array(
                 'user'=>$username
             ),[
                 'uid'=>NOW_UID
