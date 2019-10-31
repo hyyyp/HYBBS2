37512b105342b000f26ed9400ba77ecb109a24e6
krabs <krabs@live.cn>
2019-11-01 00:49:09 +0800

    升级版本号

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ if(version_compare(PHP_VERSION,'5.4.0','<')){
 	die('HYPHP2.0 不支持 5.4以下的PHP版本.  当前你的PHP版本：' . PHP_VERSION);
 }
 
-define('HYBBS_V'			,'2.2.10');
+define('HYBBS_V'			,'2.3');
 define('INDEX_PATH' 		, str_replace('\\', '/', dirname(__FILE__)).'/');
 define('DEBUG'      ,(is_file(INDEX_PATH . 'DEBUG'))?false:true);
 define('PLUGIN_ON'  ,true);
@@ -13,42 +13,32 @@ define('PLUGIN_MORE_LANG_ON',true);
 
 require  'HY/HYPHP.php';
 /*
+优化 - 加入精华帖
+优化 - 支持用户修改用户名
+优化 - 增加收藏帖子
+优化 - 后台主题评论页面 优化时间显示 显示最后编辑时间 最后编辑人
 
-修复 - 编辑帖子目录不存在的问题
-修复 - 框架缓存类库一处代码没有正确判断缓存是否存在而删除的问题
-修复 - Post 编辑帖子处理删除旧文件时 没有正确判断旧文件是否存在的问题
-修复 - MYSQL8.0无法使用HYBBS的问题
-修复 - 后台无法一键删除用户所有子评论、点评的问题
-修复 - 后台用户一键删除主题、删除评论，没有删除附件的问题
-修复 - 后台用户一键删除所有上传文件，增加删除主题，评论附件。
-修复 - 后台删除用户，没有删除干净附件
+修复 - 后台删除评论 删除附带附件时产生的额外的目录
+
+框架 - Medoo1.4版本升级到1.7.6
+框架 - DEBUG_PAGE 优化数据库操作记录，显示的SQL语句可以直接拿到数据库执行。		
+
+ALTER TABLE `hy_thread` ADD COLUMN `digest` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否精华' AFTER `hide`;
+
+CREATE TABLE if not exists `hy_thread_star` (
+  `uid` int(10) UNSIGNED NOT NULL,
+  `tid` int(10) UNSIGNED NOT NULL,
+  `atime` int(10) UNSIGNED NOT NULL,
+  UNIQUE KEY `uid_tid` (`uid`,`tid`),
+  INDEX `atime` (`atime`)
+) ENGINE = #SQL_STORAGE_ENGINE# DEFAULT CHARSET=utf8;
 
 
-ALTER TABLE `hy_fileinfo` CHANGE `gold` `gold` INT(10) UNSIGNED NOT NULL DEFAULT '0';
-ALTER TABLE `hy_fileinfo` CHANGE `hide` `hide` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0';
-ALTER TABLE `hy_fileinfo` CHANGE `downs` `downs` INT(10) UNSIGNED NOT NULL DEFAULT '0';
-ALTER TABLE `hy_fileinfo` CHANGE `mess` `mess` TEXT NULL DEFAULT NULL;
-
-ALTER TABLE `hy_forum` CHANGE `forumg` `forumg` TEXT NULL DEFAULT NULL;
-ALTER TABLE `hy_forum` CHANGE `json` `json` TEXT NULL DEFAULT NULL;
-ALTER TABLE `hy_forum` CHANGE `html` `html` LONGTEXT NULL DEFAULT NULL;
-ALTER TABLE `hy_forum` CHANGE `color` `color` VARCHAR(30) NOT NULL DEFAULT '';
-ALTER TABLE `hy_forum` CHANGE `background` `background` VARCHAR(30) NOT NULL DEFAULT '';
-
-ALTER TABLE `hy_log` CHANGE `gold` `gold` INT(11) NOT NULL DEFAULT '0';
-ALTER TABLE `hy_log` CHANGE `credits` `credits` INT(11) NOT NULL DEFAULT '0';
-ALTER TABLE `hy_log` CHANGE `content` `content` VARCHAR(32) NOT NULL DEFAULT '';
-
-ALTER TABLE `hy_user` CHANGE `threads` `threads` INT(10) UNSIGNED NOT NULL DEFAULT '0';
-ALTER TABLE `hy_user` CHANGE `posts` `posts` INT(10) UNSIGNED NOT NULL DEFAULT '0';
-ALTER TABLE `hy_user` CHANGE `post_ps` `post_ps` INT(10) UNSIGNED NOT NULL DEFAULT '0';
-
-ALTER TABLE `hy_usergroup` CHANGE `font_css` `font_css` LONGTEXT NULL DEFAULT NULL;
-ALTER TABLE `hy_usergroup` CHANGE `json` `json` TEXT NULL DEFAULT NULL;
+ALTER TABLE `hy_thread` ADD `recycling` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' AFTER `state`;
+ALTER TABLE `hy_thread` DROP `recycling`;
 
 
 
-收藏帖子
 
 重新部署 互联登陆系统 统一页面 (例如 QQ 微信 等第三方登陆 统一页面)
 重新部署 充值系统 统一页面
