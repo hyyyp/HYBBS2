a9f8b8ee343af9f1797a11e33de1d90f5681ef6b
krabs <krabs@live.cn>
2019-12-03 11:50:12 +0800

    修改版本号

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ if(version_compare(PHP_VERSION,'5.4.0','<')){
 	die('HYPHP2.0 不支持 5.4以下的PHP版本.  当前你的PHP版本：' . PHP_VERSION);
 }
 
-define('HYBBS_V'			,'2.3.1');
+define('HYBBS_V'			,'2.3.2');
 define('INDEX_PATH' 		, str_replace('\\', '/', dirname(__FILE__)).'/');
 define('DEBUG'      ,(is_file(INDEX_PATH . 'DEBUG'))?false:true);
 define('PLUGIN_ON'  ,true);
@@ -14,6 +14,7 @@ define('PLUGIN_MORE_LANG_ON',true);
 require  'HY/HYPHP.php';
 /*
 
+archiver
 
 重新部署 互联登陆系统 统一页面 (例如 QQ 微信 等第三方登陆 统一页面)
 重新部署 充值系统 统一页面
